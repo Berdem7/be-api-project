e954ba71ea7d91827718773e26cd843f74e86b8c
mstars_lab2_07 <mstars_lab2_07@mstars-lab2-07s-iMac.local>
2022-06-09 14:01:38 +0800

    Middleware

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ const CategoryController = require("../controller/CategoryController");
 const FoodController = require("../controller/FoodController");
 const UserController = require("../controller/UserController");
 const Authentication = require("../controller/Authentication");
+const auth = require("../middleware/Auth");
 
 //Categories
 router.get("/category", CategoryController.get_categories);
@@ -24,11 +25,11 @@ router.delete("/foods/:id", FoodController.delete_foods);
 router.put("/foods", jsonParser, FoodController.update_foods);
 
 //Users
-router.get("/users/:name", UserController.find_users);
-router.get("/users", UserController.get_users);
-router.post("/users", jsonParser, UserController.create_users);
-router.delete("/users/:id", UserController.delete_users);
-router.put("/users", jsonParser, UserController.update_users);
+router.get("/users/:name", auth, UserController.find_users);
+router.get("/users", auth, UserController.get_users);
+router.post("/users", auth, jsonParser, UserController.create_users);
+router.delete("/users/:id", auth, UserController.delete_users);
+router.put("/users", auth, jsonParser, UserController.update_users);
 router.post("/users/register", Authentication.register);
 router.post("/users/login", Authentication.login);
 
